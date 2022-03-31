4b55f23da8c610bca0f221b190981d53ddb8c581
Ev <59845999+just-evv@users.noreply.github.com>
2022-03-31 13:38:09 -0700

    add rollbar

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-test
+## Task Manager
+[![Test Coverage](https://api.codeclimate.com/v1/badges/685b8ed1772fc9801d39/test_coverage)](https://codeclimate.com/github/just-evv/php-project-lvl4/test_coverage) 
+
+
--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Misc
+         */
+        Rollbar\Laravel\RollbarServiceProvider::class,
     ],
 
     /*
