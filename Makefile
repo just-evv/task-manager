30dae75069c73c31d56f197b8d33d852ac62a541
Ev <ev.sivkova@outlook.com>
2022-04-11 12:05:12 -0700

    add task_status

--- Makefile
+++ Makefile
@@ -17,6 +17,8 @@ setup:
 	composer install
 	cp -n .env.example .env|| true
 	php artisan key:gen --ansi
+	php artisan migrate
+	php artisan db:seed
 	npm install
 
 test-coverage:
---
+++ database/seeders/TaskStatusSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TaskStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('task_statuses')->insert([
+                ['name' => 'new'],
+                ['name' => 'in process'],
+                ['name' => 'testing'],
+                ['name' => 'completed']
+        ]);
+    }
+}
