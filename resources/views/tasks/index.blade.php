d0e3af366ac523ae067d064d24573a6a866f196c
Ev <ev.sivkova@outlook.com>
2022-05-10 18:05:29 -0700

    add index

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    @include('flash::message')
     <h1 class="mb-5">Tasks</h1>
     <div class="d-flex mb-3">
         @can('create-task')
@@ -20,18 +21,26 @@
             <th>Created by</th>
             <th>Assigned to</th>
             <th>Created at</th>
+            @canany(['edit-task'], $tasks)
+                <th>Action</th>
+            @endcanany
         </tr>
         </thead>
-        <tr>
-            <td></td>
-            <td></td>
-            <td>
-                <a class="text-decoration-none" href=" { route('') } ">
-                </a>
-            </td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
+        @foreach ($tasks as $task)
+            <tr>
+                <td>{{ $task->id }}</td>
+                <td>{{ $task->status->name }}</td>
+                <td>{{ $task->name }}</td>
+                <td>{{ $task->creator->name }}</td>
+                <td>{{ $task->assignedUser->name ?? ''}}</td>
+                <td>{{ $task->created_at->toDateString() }}</td>
+                @canany(['edit-task'], $task)
+                    <td>
+                        <a class="text-decoration-none" href="{{ route('tasks.edit', $task) }}">Edit</a>
+                    </td>
+                @endcanany
+            </tr>
+        @endforeach
     </table>
+    {{ $tasks->links('pagination::bootstrap-4') }}
 @endsection
