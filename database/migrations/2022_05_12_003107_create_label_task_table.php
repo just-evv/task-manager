e0c94c45e842e2f3bded868038d5f0b34bd0d495
Ev <ev.sivkova@outlook.com>
2022-05-31 15:56:08 -0700

    cascade delete migrations

--- database/migrations/2022_05_12_003107_create_label_task_table.php
+++ database/migrations/2022_05_12_003107_create_label_task_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->bigInteger('label_id');
             $table->foreign('label_id')->references('id')->on('labels');
             $table->bigInteger('task_id');
-            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
             $table->timestamps();
         });
     }
