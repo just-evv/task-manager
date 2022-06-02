1e1f8c1f4b247c9cb5e3716b52d60fd61e059c5e
Ev <ev.sivkova@outlook.com>
2022-06-02 11:24:38 -0700

    error ru

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -100,7 +100,10 @@ return [
     'not_in' => 'The selected :attribute is invalid.',
     'not_regex' => 'The :attribute format is invalid.',
     'numeric' => 'The :attribute must be a number.',
-    'password' => 'The password is incorrect.',
+    'password' => [
+        'wrong' => 'The password is incorrect.',
+        'min' => 'The password must be at least :min characters long',
+        'confirmed' => ''],
     'present' => 'The :attribute field must be present.',
     'prohibited' => 'The :attribute field is prohibited.',
     'prohibited_if' => 'The :attribute field is prohibited when :other is :value.',
--- lang/ru/auth.php
+++ lang/ru/auth.php
@@ -1,20 +1,18 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Authentication Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used during authentication for various
+| messages that we need to display to the user. You are free to modify
+| these language lines according to your application's requirements.
+|
+*/
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
-    'failed' => 'Введите правильные имя пользователя и пароль',
-    'password' => 'Введите правильные имя пользователя и пароль',
-    'throttle' => 'Слишком много попыток залогиниться. Повторите попытку через :seconds секунд.',
-
+    'failed'   => 'Неверное имя пользователя или пароль.',
+    'password' => 'Неверный пароль.',
+    'throttle' => 'Слишком много попыток входа. Пожалуйста, попробуйте еще раз через :seconds секунд.',
 ];
