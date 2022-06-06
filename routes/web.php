2d5a86bbb11820395a8f082eea98c12c5d1168c8
Ev <ev.sivkova@outlook.com>
2022-06-06 11:57:24 -0700

    package-lock.json upd

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    Log::debug('Test debug message');
+    //Log::debug('Test debug message');
     return view('index');
 })->name('index');
 
